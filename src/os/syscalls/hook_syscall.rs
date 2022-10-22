3806db2fe2ae5ea9258089a1a1212e8442733502
Marek G <marek-dev@yandex.com>
2022-10-22 15:28:47 +0200

    Elf loader

--- src/os/syscalls/hook_syscall.rs
+++ src/os/syscalls/hook_syscall.rs
@@ -2,25 +2,24 @@ use crate::emulator::context::Context;
 use unicorn_engine::{RegisterARM, Unicorn};
 
 pub fn hook_syscall(unicorn: &mut Unicorn<Context>, intno: u32) {
-    /*let pc = uc.reg_read(RegisterARM64::PC as i32).unwrap();
-    let syscall = get_syscall(uc);
-    unicorn.syscall(syscall);*/
-    log::debug!(
-        "{:#x}: int {} syscall #{}, arg1: {:#x}, arg2: {:#x}, arg3: {:#x}",
-        unicorn.reg_read(RegisterARM::PC).unwrap(),
-        intno,
-        unicorn.reg_read_i32(RegisterARM::R7).unwrap(),
-        unicorn.reg_read_i32(RegisterARM::R0).unwrap(),
-        unicorn.reg_read_i32(RegisterARM::R1).unwrap(),
-        unicorn.reg_read_i32(RegisterARM::R2).unwrap(),
-    );
+    let syscall_number = unicorn.reg_read_i32(RegisterARM::R7).unwrap();
 
-    /*let res = if unicorn.reg_read_i32(RegisterARM::R2).unwrap() == 0x17 {
-        0x4738d000
-    } else {
-        0
-    };*/
-    let res = 0x17000;
+    let res = match syscall_number {
+        //45 => 0u32,
+        _ => {
+            panic!(
+                "{:#x}: not implemented syscall #{} (int {}), args: {:#x}, {:#x}, {:#x}, ...",
+                unicorn.reg_read(RegisterARM::PC).unwrap(),
+                unicorn.reg_read_i32(RegisterARM::R7).unwrap(),
+                intno,
+                unicorn.reg_read_i32(RegisterARM::R0).unwrap(),
+                unicorn.reg_read_i32(RegisterARM::R1).unwrap(),
+                unicorn.reg_read_i32(RegisterARM::R2).unwrap(),
+            );
+        }
+    };
 
-    unicorn.reg_write(RegisterARM::R0 as i32, res).unwrap();
+    unicorn
+        .reg_write(RegisterARM::R0 as i32, res as u64)
+        .unwrap();
 }
