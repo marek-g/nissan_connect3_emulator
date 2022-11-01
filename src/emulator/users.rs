e2aa388e8732158e09c00aa2363f40f396ce5408
Marek G <marek-dev@yandex.com>
2022-11-01 18:56:54 +0100

    Include unicorn bindings in the workspace

--- src/emulator/users.rs
+++
@@ -1,4 +0,0 @@
-pub const UID: u32 = 1000;
-pub const EUID: u32 = 1000;
-pub const GID: u32 = 1001;
-pub const EGID: u32 = 1001;
--- src/os/mod.rs
+++
@@ -1,4 +0,0 @@
-mod syscalls;
-
-pub use syscalls::hook_syscall::hook_syscall;
-pub use syscalls::sys_calls_state::SysCallsState;
